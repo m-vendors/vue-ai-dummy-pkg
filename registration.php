a4c520fb74850b03e12092d3440926ac1c3bd2d6
M Vendors <m-vendors@protonmail.com>
2021-05-23 21:34:15 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'vue-ai_DummyPkg',
+    __DIR__
+);
